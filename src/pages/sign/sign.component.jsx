802a6640bd6fea11e32d2b0024f00d5f3f2c07a1
zura gogoladze <gogoladze.zura@outlook.com>
2021-06-08 23:10:18 +0400

    Added sign up with email and password

--- src/pages/sign/sign.component.jsx
+++ src/pages/sign/sign.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign.styles.scss';
 
-const SignPage = () => <SignIn />;
+const SignPage = () => (
+	<div className='sign'>
+		<SignIn />
+		<SignUp />
+	</div>
+);
 
 export default SignPage;
